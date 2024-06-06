eb69de835294a95e6daf9b775df4e880b5f5d6a4
Yaroslav Lomtev <79333072+Yaroslav0820@users.noreply.github.com>
2024-06-06 12:11:57 +0300

    task-3

--- js/task-3.js
+++ js/task-3.js
@@ -1,14 +1,15 @@
-function getElementWidth(content, padding, border){
-
-    content = parseFloat(content)
-    padding = parseFloat(padding)
-    border = parseFloat(border)
-
-
-     return content + (padding + border) * 2;
-    
+function checkForSpam(message){
+    if (message.toLowerCase().includes("spam") || message.toLowerCase().includes("sale")){
+        return true;
+    } else {
+        return false; 
+    }
 }
 
-console.log(getElementWidth("50px", "8px", "4px")); // 74
-console.log(getElementWidth("60px", "12px", "8.5px")); // 101
-console.log(getElementWidth("200px", "0px", "0px")); // 200
+console.log(checkForSpam("Latest technology news")); // false
+console.log(checkForSpam("JavaScript weekly newsletter")); // false
+console.log(checkForSpam("Get best sale offers now!")); // true
+console.log(checkForSpam("Amazing SalE, only tonight!")); // true
+console.log(checkForSpam("Trust me, this is not a spam message")); // true
+console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!")); // true
+console.log(checkForSpam("[SPAM] How to earn fast money?")); // true
